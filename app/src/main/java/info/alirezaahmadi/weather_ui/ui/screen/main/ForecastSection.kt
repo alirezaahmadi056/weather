c3fd7a43b7a426d62909cd76732d9d2a2df0b2bc
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-03-27 13:25:09 +0330

    create ForecastSection

---
+++ app/src/main/java/info/alirezaahmadi/weather_ui/ui/screen/main/ForecastSection.kt
@@ -0,0 +1,219 @@
+package info.alirezaahmadi.weather_ui.ui.screen.main
+
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.BlurredEdgeTreatment
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import info.alirezaahmadi.weather_ui.data.HourlyWeather
+import info.alirezaahmadi.weather_ui.data.WeeklyWeather
+import kotlinx.coroutines.launch
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun BoxScope.ForecastSection(
+    hourlyWeatherDta: List<HourlyWeather>,
+    weeklyWeatherData: List<WeeklyWeather>,
+) {
+    val tabs = listOf("Hourly Forecast", "Weekly Forecast")
+    val pagerState = rememberPagerState { tabs.size }
+    val coroutineScope = rememberCoroutineScope()
+    Box(
+        modifier = Modifier
+            .clip(RoundedCornerShape(topStart = 25.dp, topEnd = 25.dp))
+            .background(
+                Brush.radialGradient(
+                    listOf(
+                        Color(0xff2E335A),
+                        Color(0xff1C1B33),
+                    )
+                )
+            )
+            .align(Alignment.BottomCenter)
+    ) {
+        Column(
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .fillMaxWidth(),
+        ) {
+            Box(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 10.dp, bottom = 4.dp)
+                    .size(70.dp,4.dp)
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(Color.White,RoundedCornerShape(20.dp)),
+            )
+            TabRow(
+                selectedTabIndex = pagerState.currentPage,
+                modifier = Modifier.fillMaxWidth(),
+                containerColor = Color.Transparent,
+                contentColor = Color.White,
+                divider = {
+                    HorizontalDivider(
+                        thickness = 1.dp,
+                        color = Color.LightGray.copy(0.7f)
+                    )
+                },
+                indicator = { tabPositions ->
+                    Box(
+                        Modifier
+                            .tabIndicatorOffset(tabPositions[pagerState.currentPage])
+                            .height(4.dp)
+                            .clip(RoundedCornerShape(20.dp))
+                            .background(Color(0xff593c7a))
+                    )
+                }
+            ) {
+                tabs.forEachIndexed { index, title ->
+                    Tab(
+                        selected = pagerState.currentPage == index,
+                        onClick = {
+                            coroutineScope.launch {
+                                pagerState.animateScrollToPage(
+                                    page = index,
+                                    animationSpec = tween(600)
+                                )
+                            }
+                        },
+                        text = {
+                            Text(
+                                text = title,
+                                fontSize = 14.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = if (pagerState.currentPage == index) Color.White else Color.Gray
+                            )
+                        }
+                    )
+                }
+            }
+            HorizontalPager(
+                state = pagerState,
+            ) { page ->
+                when (page) {
+                    0 -> {
+                        HourlyForecastSection(hourlyWeatherDta)
+                    }
+
+                    1 -> {
+                        HourlyForecastSection(hourlyWeatherDta)
+                    }
+
+                    else -> {
+                        HourlyForecastSection(hourlyWeatherDta)
+                    }
+                }
+            }
+            Spacer(Modifier.height(250.dp))
+        }
+
+
+    }
+}
+
+@Composable
+fun BottomForecastSection() {
+    Box(
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Composable
+fun HourlyForecastSection(
+    hourlyData: List<HourlyWeather>,
+) {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(hourlyData) {
+            HourlyWeatherItem(it)
+        }
+    }
+}
+
+@Composable
+fun WeeklyHourlyForecastSection(
+    weeklyData: List<WeeklyWeather>,
+) {
+    LazyRow(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        items(weeklyData) {
+            //   HourlyWeatherItem(it)
+        }
+
+    }
+}
+
+@Composable
+fun HourlyWeatherItem(weather: HourlyWeather) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .width(60.dp)
+            .background(Color(0xFF4C4F69), shape = RoundedCornerShape(16.dp))
+            .padding(8.dp)
+    ) {
+        Text(
+            text = weather.hour,
+            fontSize = 14.sp,
+            color = Color.White
+        )
+        Image(
+            painter = painterResource(id = weather.iconRes),
+            contentDescription = "Weather Icon",
+            modifier = Modifier.size(32.dp)
+        )
+        Text(
+            text = "${weather.precipitationChance}%",
+            fontSize = 12.sp,
+            color = Color.Cyan
+        )
+
+        Text(
+            text = "${weather.temperature}°",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+    }
+}
