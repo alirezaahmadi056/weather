6f4fbb0bb96912b0076228c9cc670a339978a8ac
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-03-27 14:12:34 +0330

    update MainScreen

--- app/src/main/java/info/alirezaahmadi/weather_ui/ui/screen/main/MainScreen.kt
+++ app/src/main/java/info/alirezaahmadi/weather_ui/ui/screen/main/MainScreen.kt
@@ -1,13 +1,14 @@
 package info.alirezaahmadi.weather_ui.ui.screen.main
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import info.alirezaahmadi.weather_ui.R
 import info.alirezaahmadi.weather_ui.data.WeatherItem
 import kotlin.math.roundToInt
@@ -28,8 +29,9 @@ fun MainScreen() {
             painter = painterResource(R.drawable.house),
             contentDescription = "",
             modifier = Modifier
-                .align(Alignment.Center)
-                .aspectRatio(1f),
+                .padding(top = 15.dp)
+                .align(Alignment.Center),
+            contentScale = ContentScale.Fit
         )
         ForecastSection(weatherItem.hourlyForecast, weatherItem.weeklyForecast)
     }
