8b694c27bddeb9fef025343f6258ebf6295207d7
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-03-31 00:34:23 +0330

    update City and fakeCities

--- app/src/main/java/info/alirezaahmadi/weather_ui/data/City.kt
+++ app/src/main/java/info/alirezaahmadi/weather_ui/data/City.kt
@@ -4,20 +4,25 @@ import androidx.annotation.DrawableRes
 import info.alirezaahmadi.weather_ui.R
 
 data class City(
-    val name: String,
+    val location: String,
     val temperature: Int,
     val highTemp: Int,
     val lowTemp: Int,
     val condition: String,
-    @DrawableRes val imageRes: Int
+    @DrawableRes val imageRes: Int=R.drawable.cloud_rain
 ){
     companion object {
         val fakeCities = listOf(
-            City("Paris", 22, 26, 18, "Sunny", R.drawable.cloud_rain),
-            City("New York", 18, 22, 15, "Cloudy", R.drawable.cloud_rain),
-            City("Tokyo", 25, 30, 20, "Rainy", R.drawable.cloud_rain),
-            City("Sydney", 20, 24, 17, "Windy", R.drawable.cloud_rain),
-            City("London", 16, 20, 12, "Foggy", R.drawable.cloud_rain)
+            City("Montreal, Canada", 19, 24, 18, "Mid Rain"),
+            City("New York, USA", 22, 27, 20, "Sunny"),
+            City("London, UK", 16, 20, 14, "Cloudy"),
+            City("Paris, France", 18, 22, 16, "Foggy"),
+            City("Tokyo, Japan", 25, 30, 22, "Stormy"),
+            City("Toronto, Canada", 15, 19, 10, "Windy"),
+            City("Los Angeles, USA", 28, 32, 25, "Sunny"),
+            City("Berlin, Germany", 14, 18, 11, "Cloudy"),
+            City("Moscow, Russia", 10, 14, 6, "Snowy"),
+            City("Beijing, China", 22, 26, 18, "Hazy")
         )
     }
 }
