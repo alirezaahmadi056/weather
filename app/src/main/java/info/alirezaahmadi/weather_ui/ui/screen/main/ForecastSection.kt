6efa1d37064c54265bc8b47a4a2e0978529bf3d6
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-13 00:05:52 +0330

    add onClick showSheetSearch in BottomForecastSection

--- app/src/main/java/info/alirezaahmadi/weather_ui/ui/screen/main/ForecastSection.kt
+++ app/src/main/java/info/alirezaahmadi/weather_ui/ui/screen/main/ForecastSection.kt
@@ -2,7 +2,6 @@ package info.alirezaahmadi.weather_ui.ui.screen.main
 
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -11,15 +10,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
@@ -40,7 +35,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -224,7 +218,7 @@ fun BottomForecastSection(
             modifier = Modifier
                 .padding(end = 25.dp)
                 .align(Alignment.CenterEnd)
-                .clickable { },
+                .clickable(onClick = showSheetSearch),
             contentAlignment = Alignment.Center
         ) {
             Icon(
