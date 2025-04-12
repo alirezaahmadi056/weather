d9ca20b755b08e1a648bb7dcda56ae48baa0f249
Hosein Ahmadi <iamohoseinam@gmail.com>
2025-04-12 22:10:16 +0330

    update imageRes in fakeCities

--- app/src/main/java/info/alirezaahmadi/weather_ui/data/City.kt
+++ app/src/main/java/info/alirezaahmadi/weather_ui/data/City.kt
@@ -9,20 +9,19 @@ data class City(
     val highTemp: Int,
     val lowTemp: Int,
     val condition: String,
-    @DrawableRes val imageRes: Int=R.drawable.cloud_rain
+    @DrawableRes val imageRes: Int
 ){
     companion object {
         val fakeCities = listOf(
-            City("Montreal, Canada", 19, 24, 18, "Mid Rain"),
-            City("New York, USA", 22, 27, 20, "Sunny"),
-            City("London, UK", 16, 20, 14, "Cloudy"),
-            City("Paris, France", 18, 22, 16, "Foggy"),
-            City("Tokyo, Japan", 25, 30, 22, "Stormy"),
-            City("Toronto, Canada", 15, 19, 10, "Windy"),
-            City("Los Angeles, USA", 28, 32, 25, "Sunny"),
-            City("Berlin, Germany", 14, 18, 11, "Cloudy"),
-            City("Moscow, Russia", 10, 14, 6, "Snowy"),
-            City("Beijing, China", 22, 26, 18, "Hazy")
+            City("Montreal, Canada", 19, 24, 18, "Mid Rain",R.drawable.cloud_mid_rain),
+            City("New York, USA", 22, 27, 20, "Fast Wind",R.drawable.cloud_fast_wind),
+            City("London, UK", 16, 20, 14, "Cloudy",R.drawable.sun_cloud_angled_rain),
+            City("Paris, France", 18, 22, 16, "Tornado",R.drawable.tornado),
+            City("Tokyo, Japan", 25, 30, 22, "Fast Wind",R.drawable.cloud_fast_wind),
+            City("Toronto, Canada", 15, 19, 10, "Mid Rain",R.drawable.cloud_mid_rain),
+            City("Los Angeles, USA", 28, 32, 25, "Tornado",R.drawable.tornado),
+            City("Berlin, Germany", 14, 18, 11, "Cloudy",R.drawable.sun_cloud_angled_rain),
+            City("Moscow, Russia", 10, 14, 6, "Fast Wind",R.drawable.cloud_fast_wind),
         )
     }
 }
